fc82659f369bedc70c27cd1c71c0ac07793b3552
Kyle Pekosh <kylep342@gmail.com>
2022-04-14 22:04:12 -0500

    Adds moneyfunx as a dependency Adds beginning vue files for form components

---
+++ src/components/loanForm/FormItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="formItem">
+    <slot name="formLabel"></slot>
+    <slot name="formInput"></slot>
+  </div>
+</template>
+
+<style scoped>
+.formItem {
+  display: flex;
+}
+</style>
