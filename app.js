b75b46627c19486d0fc382b84835b1395d5b8d29
Kyle Pekosh <kylep342@gmail.com>
2022-07-17 16:46:59 -0500

    Old configs Adds globalMinimumPayment computed property Adds disableCreate computed property Adds deleteLoan method Adds button to loan cards to delete them

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-
+// import * as Vue from "vue";
+import * as moneyfunx from "moneyfunx";
 
 const app = Vue.createApp({
   data() {
@@ -8,26 +9,45 @@ const app = Vue.createApp({
       termInYears: null,
       loans: [],
       paymentBudgets: [],
-    }
+    };
   },
   computed: {
     paymentSchedules() {
-      return this.loans
-    }
+      return this.loans;
+    },
+    globalMinPayment() {
+      return this.loans.reduce(
+        (previousValue, currentValue) =>
+          (previousValue += currentValue.minPayment),
+        0
+      );
+    },
+    disableCreate() {
+      return !(this.principal && this.interestRate && this.termInYears);
+    },
   },
   watch: {},
   methods: {
     createLoan() {
-      const newLoan = {
-        principal: this.principal,
-        interestRate: this.interestRate,
-        termInYears: this.termInYears,
-      }
+      const principal = parseFloat(this.principal);
+      const interestRate = parseFloat(this.interestRate) / 100;
+      const termInYears = parseFloat(this.termInYears);
+
+      const newLoan = new moneyfunx.Loan(
+        principal,
+        interestRate,
+        12,
+        termInYears
+      );
+
       console.log(`New loan: ${JSON.stringify(newLoan)}`);
       this.loans.push(newLoan);
       this.principal = null;
       this.interestRate = null;
       this.termInYears = null;
+    },
+    deleteLoan(id) {
+      this.loans = this.loans.filter((loan) => loan.id != id);
     },
   },
 });
