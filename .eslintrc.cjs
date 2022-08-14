1c6806fbd579aa2f73de3d592d301e3584d07581
Kyle Pekosh <kylep342@gmail.com>
2022-08-14 13:38:02 -0500

    Resets build setup locally to work Migrates (a bit) towards modern Vue patterns

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,11 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
-
 module.exports = {
-  root: true,
-  extends: [
+  "root": true,
+  "extends": [
     "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier",
+    "eslint:recommended"
   ],
-  env: {
-    "vue/setup-compiler-macros": true,
-  },
-};
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  }
+}
