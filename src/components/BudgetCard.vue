e23a058de8dc1955d2add646426b3c788869c3de
Kyle Pekosh <kylep342@gmail.com>
2022-11-08 06:58:27 -0600

    Adds BudgetCard component

---
+++ src/components/BudgetCard.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: ["budget", "index", "deleteBudget"]
+  }
+</script>
+
+<template>
+  <div :class="['cardHeader', 'header']">
+    <p class="cardHeaderTitle">
+      Monthly Budget {{ this.index }}
+    </p>
+    <div class="cardHeaderButtonContainer">
+      <button :class="['exitButton', 'bold']" @click="this.deleteBudget(this.budget)">x</button>
+    </div>
+  </div>
+  <div class="cardBody">
+    <h2>${{ budget }}/month</h2>
+  </div>
+</template>
--- src/components/BudgetsPanel.vue
+++ src/components/BudgetsPanel.vue
@@ -1,16 +1,17 @@
 <script>
+import BudgetCard from './BudgetCard.vue';
+
 export default {
-   props: ["budgets", "deleteBudget"],
+    props: ["budgets", "deleteBudget"],
+    components: { BudgetCard }
 }
 </script>
 
 <template>
   <div id="budgetsPanel" v-show="this.budgets.length">
     <ul class="cardHolder">
-      <li v-for="budget in this.budgets" :key="budget.id" class="card">
-        {{ budget }}
-        <br />
-        <button @click="this.deleteBudget(budget)">X</button>
+      <li v-for="(budget, index) in this.budgets" :key="budget.id" class="card">
+        <BudgetCard :budget="budget" :index="index + 1" :deleteBudget="this.deleteBudget" />
       </li>
     </ul>
   </div>
