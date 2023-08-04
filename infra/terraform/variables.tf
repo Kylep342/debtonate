0fec8d2ab3e49f70a804111f9871699b8f7b5c2c
Kyle Pekosh <kylep342@gmail.com>
2023-08-04 12:12:00 -0500

    Add tf for load balancer (#5)
    * Adds code for load balancing in front of bucket to provide HTTPS
    
    * Adds new tf files

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,21 +1,38 @@
 variable "project_id" {
-    description = "GCP Project ID hosting all cloud resources"
-    default = "debtonate"
+  description = "GCP Project ID hosting all cloud resources"
+  default = "debtonate"
 }
 
 variable "region" {
-    default = "us-central1"
+  default = "us-central1"
 }
 
 variable "primary_zone" {
-    default = "us-central1-c"
+  default = "us-central1-c"
 }
 
 variable "gcp-credentials" {
-    description = "Token to authenticate with Google"
-    default = ""
+  description = "Token to authenticate with Google"
+  default = ""
 }
 
 variable "organization_name" {
-    default = "debtonate"
+  default = "debtonate"
+}
+
+
+variable "group1_region" {
+  default = "us-west1"
+}
+
+variable "group2_region" {
+  default = "us-central1"
+}
+
+variable "group3_region" {
+  default = "us-east1"
+}
+
+variable "network_name" {
+  default = "debtonate-https-network"
 }
