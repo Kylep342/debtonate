f0111b7bc9dcaa7afd502363c607704e4010f09f
Kyle Pekosh <kylep342@gmail.com>
2023-08-04 12:09:26 -0500

    Adds code for load balancing in front of bucket to provide HTTPS

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -23,3 +23,81 @@ resource "google_storage_bucket_access_control" "public_rule" {
   role   = "READER"
   entity = "allUsers"
 }
+
+resource "google_compute_network" "default" {
+  name                    = var.network_name
+  auto_create_subnetworks = "false"
+}
+
+resource "google_compute_subnetwork" "group1" {
+  name                     = var.network_name
+  ip_cidr_range            = "10.125.0.0/20"
+  network                  = google_compute_network.default.self_link
+  region                   = var.group1_region
+  private_ip_google_access = true
+}
+
+locals {
+  health_check = {
+    request_path = "/"
+    port         = 80
+  }
+}
+
+# [START cloudloadbalancing_ext_http_gce_plus_bucket]
+module "gce-lb-https" {
+  source  = "GoogleCloudPlatform/lb-http/google"
+  name    = var.network_name
+  project = var.project_id
+  target_tags = []
+  firewall_networks = [google_compute_network.default.self_link]
+  url_map           = google_compute_url_map.debtonate-https-network.self_link
+  create_url_map    = false
+  ssl               = true
+  private_key       = tls_private_key.debtonate.private_key_pem
+  certificate       = tls_self_signed_cert.debtonate.cert_pem
+
+  backends = {
+    default = {
+      protocol    = "HTTP"
+      port        = 80
+      port_name   = "http"
+      timeout_sec = 10
+      enable_cdn  = false
+      groups      = []
+      health_check = local.health_check
+      log_config = {
+        enable      = true
+        sample_rate = 1.0
+      }
+
+      iap_config = {
+        enable = false
+      }
+    }
+  }
+}
+
+resource "google_compute_url_map" "debtonate-https-network" {
+  // note that this is the name of the load balancer
+  name            = var.network_name
+  default_service = module.gce-lb-https.backend_services["default"].self_link
+
+  host_rule {
+    hosts        = ["*"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name            = "allpaths"
+    default_service = module.gce-lb-https.backend_services["default"].self_link
+
+    path_rule {
+      paths = [
+        "/",
+        "/*"
+      ]
+      service = google_storage_bucket.static_website.self_link
+    }
+  }
+}
--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -1,7 +1,14 @@
 #
 provider "google" {
-    project     = var.project_id
-    region      = var.region
-    zone        = var.primary_zone
-    credentials = var.gcp-credentials
+  project     = var.project_id
+  region      = var.region
+  zone        = var.primary_zone
+  credentials = var.gcp-credentials
+}
+
+provider "google-beta" {
+  project     = var.project_id
+  region      = var.region
+  zone        = var.primary_zone
+  credentials = var.gcp-credentials
 }
