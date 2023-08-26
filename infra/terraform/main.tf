dc1f564023971acb07dfc5aa2f458ca610d591be
Kyle Pekosh <kylep342@gmail.com>
2023-08-26 12:41:28 -0500

    Switches scheme to match load balancer

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -121,7 +121,7 @@ resource "google_compute_target_http_proxy" "default" {
 resource "google_compute_global_forwarding_rule" "default" {
   name                  = "http-lb-forwarding-rule"
   ip_protocol           = "TCP"
-  load_balancing_scheme = "EXTERNAL_MANAGED"
+  load_balancing_scheme = "EXTERNAL"
   port_range            = "80"
   target                = google_compute_target_http_proxy.default.id
   ip_address            = google_compute_global_address.default.id
