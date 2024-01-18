effc288ba9b54db64b69db285536f07f07e40198
Kyle Pekosh <kylep342@gmail.com>
2024-01-17 19:03:50 -0600

    Several changes (#32)
    Adds a DEFAULT constant for the id used for the default Budget
    Puts the minimum budget card at the top of the BudgetPanel

--- src/components/BudgetsPanel.vue
+++ src/components/BudgetsPanel.vue
@@ -1,5 +1,6 @@
 <script>
 import BudgetCard from './BudgetCard.vue';
+import constants from '../constants/constants';
 
 export default {
   props: [
@@ -10,19 +11,31 @@ export default {
     'viewBudget',
   ],
   components: { BudgetCard },
+  computed: {
+    defaultBudgetIndex() {
+      return this.budgets.findIndex((budget) => budget.id === constants.DEFAULT);
+    },
+    orderedBudgets() {
+      return [
+        this.budgets[this.defaultBudgetIndex],
+        ...this.budgets.slice(0, this.defaultBudgetIndex),
+        ...this.budgets.slice(this.defaultBudgetIndex + 1),
+      ];
+    },
+  },
 };
 </script>
 
 <template>
   <div :class="['panel']" v-show='this.budgets.length'>
     <ul :class="['cardHolder']">
-      <li v-for='(budget, index) in this.budgets' :key='budget.id' :class="['card']">
+      <li v-for='(budget, index) in this.orderedBudgets' :key='budget.id' :class="['card']">
         <BudgetCard
           :budget='budget'
           :budgetTotals='this.budgetsTotals[budget.id]'
           :deleteBudget='this.deleteBudget'
           :editBudget='this.editBudget'
-          :index='index + 1'
+          :index='index'
           :viewBudget='this.viewBudget'
         />
       </li>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,11 +1,13 @@
 // file for constants
 
 const BUDGET = 'Budget';
+const DEFAULT = 'default';
 const LOAN = 'Loan';
 const TOTALS = 'totals';
 
 export default Object.freeze({
   BUDGET,
+  DEFAULT,
   LOAN,
   TOTALS,
 });
