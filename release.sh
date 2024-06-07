27c140cf57b3d421b6eae7a9cdb68a16923d956f
Kyle Pekosh <kylep342@gmail.com>
2024-06-07 08:58:12 -0500

    Updates

--- release.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-jq -r .version package.json | xargs git tag && git push && git push --tags
--- src/components/ui/BaseCard.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div :class="['card']">
-    <slot name="cardTitle"></slot>
-    <div :class="['card-body', 'p-4', 'overflow-y-contain']">
-      <slot name="cardBody"></slot>
-    </div>
-    <slot name="cardActions"></slot>
-  </div>
-</template>
