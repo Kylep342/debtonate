ea7919de9b1a9b1f9bf28c0f268446737a980fa3
Kyle Pekosh <kylep342@gmail.com>
2024-09-16 08:23:58 -0500

    Adds a widthRestOfViewport func

--- src/functions/viewport.js
+++ src/functions/viewport.js
@@ -4,3 +4,10 @@ export const heightRestOfViewport = (componentRef) => {
   const availableHeight = window.innerHeight - containerTop;
   return availableHeight;
 };
+
+//
+export const widthRestOfViewport = (componentRef) => {
+  const containerLeft = componentRef.value.getBoundingClientRect().left;
+  const availableWidth = window.innerWidth - containerLeft;
+  return availableWidth;
+};
