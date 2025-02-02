1e01036f7c5f996da2e850d549ead0b814c7ead5
Kyle Pekosh <kylep342@gmail.com>
2025-02-02 17:51:55 -0600

    Adds more types, fixes some type errors, updates types from moneyfunx, removes useless refs, adds '!'s to ensure no-undefined returns

--- src/types/core.ts
+++ src/types/core.ts
@@ -1,5 +1,17 @@
+import {
+  ILoan,
+  LoansPaymentSchedule
+} from 'moneyfunx';
+
 export type MonthlyBudget = {
   id: string;
   relative: number;
   absolute: number;
 }
+
+export type PaymentSchedule = {
+  paymentAmount: number;
+  paymentSchedule: LoansPaymentSchedule;
+};
+
+export type RefinancingScenarios = Record<string, ILoan[]>;
