c20772dd3494ca5bb3e4a60ba1e6514abbb8e9b6
Kyle Pekosh <kylep342@gmail.com>
2025-02-07 13:21:03 -0600

    Semicolons

--- src/types/core.ts
+++ src/types/core.ts
@@ -3,13 +3,13 @@ import { LoansPaymentSchedule } from 'moneyfunx';
 export type Budget = {
   id: string;
   relative: number;
-}
+};
 
 export type MonthlyBudget = {
   id: string;
   relative: number;
   absolute: number;
-}
+};
 
 export type PaymentSchedule = {
   paymentAmount: number;
