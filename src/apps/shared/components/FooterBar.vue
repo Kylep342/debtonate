7c34b826254c78729f38cf611e11bd168741aaa0
Kyle Pekosh <kylep342@gmail.com>
2025-09-06 11:52:28 +0900

    Adds a link to source

--- src/apps/shared/components/FooterBar.vue
+++ src/apps/shared/components/FooterBar.vue
@@ -3,12 +3,21 @@
     :id="'footer-bar'"
     :class="['bg-secondary', 'sticky', 'bottom-0', 'z-30']"
   >
-    <div :class="['flex-1', 'text-right', 'pr-4']">
-      <a
-        href="https://forms.gle/5hdgxA4jKGQA5U8J9"
-        target="_blank"
-      >report a bug</a>
-      <br>
+    <div :class="['flex']">
+      <div :class="['flex-0', 'text-left', 'pl-4']">
+        <a
+          href="https://github.com/Kylep342/debtonate"
+          target="_blank"
+        >source</a>
+        <br>
+      </div>
+      <div :class="['flex-1', 'text-right', 'pr-4']">
+        <a
+          href="https://forms.gle/5hdgxA4jKGQA5U8J9"
+          target="_blank"
+        >report a bug</a>
+        <br>
+      </div>
     </div>
   </footer>
 </template>
